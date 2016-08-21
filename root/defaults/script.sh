f2138b7d3de93d7f50696a1430f1e9ab5bc1e129
sparklyballs <sparklyballs@gmail.com>
2016-08-21 04:31:03 +0100

    add page to clone command, get's all the repos, covered up until we reach 300+ repos

--- root/defaults/script.sh
+++ root/defaults/script.sh
@@ -9,7 +9,7 @@ REPO_USER=""
 # delete folders older than 20 days
 for i in $(find /backup/ -maxdepth 1 -type d -mtime +20 -print); do echo -e "Deleting directory $i";rm -rf $i; done
 
-# make today's folder
+# make today's folders
 if [ -d "${FOLDER_MAKE}" ]; then
 rm -rf "${FOLDER_MAKE}"
 fi
@@ -17,7 +17,9 @@ mkdir -p "${FOLDER_MAKE}" && touch "${FOLDER_MAKE}"
 
 # clone git repositories into todays folder
 cd "${FOLDER_MAKE}"
-curl -s "https://api.github.com/users/${REPO_USER}/repos?per_page=200" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["clone_url"])'
+curl -s "https://api.github.com/users/${REPO_USER}/repos?page=1&per_page=100" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["clone_url"])'
+curl -s "https://api.github.com/users/${REPO_USER}/repos?page=2&per_page=100" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["clone_url"])'
+curl -s "https://api.github.com/users/${REPO_USER}/repos?page=3&per_page=100" | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["clone_url"])'
 
 # permissions
 chown -R abc:abc "${FOLDER_MAKE}"
